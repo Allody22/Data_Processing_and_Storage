b54d4378c21dffe05ed05d0b7e3aa7577a4f6ab9
IAMNO <m.bogdanov2@g.nsu.ru>
2023-09-17 19:52:02 +0700

    Решение второго задания

--- JavaConcurrency/src/main/java/ru/nsu/Task2.java
+++ JavaConcurrency/src/main/java/ru/nsu/Task2.java
@@ -1,6 +1,6 @@
 package ru.nsu;
 
-public class Task1 {
+public class Task2 {
 
     public static void main(String[] args) {
         // Описание задачи для новой нити
@@ -15,6 +15,19 @@ public class Task1 {
 
         // Запуск новой нити
         thread.start();
+
+
+        // Ожидание завершения новой нити перед продолжением главной нити
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            // Для больших проектов лучше использоваться логирования для отлавливания таких ошибок.
+            // Сам IntelliJ об этом предупреждает
+            e.printStackTrace();
+        }
+
+        System.out.println("Новая нить закончила");
+
 
         // Вывод строки из главной нити (потока)
         for (int i = 0; i < 10; ++i) {
