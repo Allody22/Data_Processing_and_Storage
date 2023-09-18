bc826c9bd54a6e4c556f2fd44f0e864e3171d923
IAMNO <m.bogdanov2@g.nsu.ru>
2023-09-18 21:15:17 +0700

    Добавление переменной для обозначения цикла. Изменение имени пакета

--- JavaConcurrency/src/main/java/ru/nsu/bogdanov/Task2.java
+++ JavaConcurrency/src/main/java/ru/nsu/bogdanov/Task2.java
@@ -1,11 +1,14 @@
-package ru.nsu;
+package ru.nsu.bogdanov;
 
 public class Task2 {
 
     public static void main(String[] args) {
+
+        final int LOOP_TASK2_ITERATOR = 10;
+
         // Описание задачи для новой нити
         Runnable task = () -> {
-            for (int i = 0; i < 10; ++i) {
+            for (int i = 0; i <  LOOP_TASK2_ITERATOR; ++i) {
                 System.out.println("Элемент цикла номер :" + i + " от новой нити");
             }
         };
@@ -15,7 +18,6 @@ public class Task2 {
 
         // Запуск новой нити
         thread.start();
-
 
         // Ожидание завершения новой нити перед продолжением главной нити
         try {
@@ -28,9 +30,8 @@ public class Task2 {
 
         System.out.println("Новая нить закончила");
 
-
         // Вывод строки из главной нити (потока)
-        for (int i = 0; i < 10; ++i) {
+        for (int i = 0; i <  LOOP_TASK2_ITERATOR; ++i) {
             System.out.println("Элемент цикла номер :" + i + " от главной нити");
         }
     }
